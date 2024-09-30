2e506932f86dac935a68f984e7e76caed064ba79
Arya Rakha <33478892+arrakh@users.noreply.github.com>
2024-09-30 12:05:27 +0100

    fixed keyboard not submitting open close correctly

--- Assets/Scripts/CrazyPhone/Input/KeyboardPhoneInput.cs
+++ Assets/Scripts/CrazyPhone/Input/KeyboardPhoneInput.cs
@@ -13,17 +13,15 @@ namespace CrazyPhone.Input
             foreach (var (number, numString) in PhoneMappings.KeycodeMappings)
             {
                 if (UnityEngine.Input.GetKeyDown(number)) ProcessDownKey(numString);
-
-                if (!UnityEngine.Input.GetKeyUp(number)) continue;
-                if (!currentlyPressed.Contains(numString)) continue;
-                currentlyPressed.Remove(numString);
-                releasedThisFrame.Add(numString);
+                if (UnityEngine.Input.GetKeyUp(number)) ProcessUpKey(numString);
             }
         }
 
         private void DetectSpacebar()
         {
             isClosed = UnityEngine.Input.GetKey(KeyCode.Space);
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space)) ProcessDownKey(CLOSE);
+            if (UnityEngine.Input.GetKeyUp(KeyCode.Space)) ProcessUpKey(OPEN);
         }
 
         private void ProcessDownKey(string numString)
@@ -31,6 +29,13 @@ namespace CrazyPhone.Input
             currentlyPressed.Add(numString);
             thisFrame.Add(numString);
             onKeyDown?.Invoke(numString);
+        }
+
+        private void ProcessUpKey(string numString)
+        {
+            if (!currentlyPressed.Contains(numString)) return;
+            currentlyPressed.Remove(numString);
+            releasedThisFrame.Add(numString);
         }
     }
 }
